2e007cc771a1a75fdaa12c366cb93dce39208b81
Typhainecp <claudepierre.pro@gmail.com>
2025-10-20 10:39:19 +0200

    Create Operation.java

---
+++ Operation.java
@@ -0,0 +1,33 @@
+package fr.formation.spring.calculatrice;
+
+public class Operation {
+
+  private String operation;
+  private int nombre1;
+  private int nombre2;
+
+
+  public String getOperation() {
+    return operation;
+  }
+
+  public void setOperation(String operation) {
+    this.operation = operation;
+  }
+
+  public int getNombre1() {
+    return nombre1;
+  }
+
+  public void setNombre1(int nombre1) {
+    this.nombre1 = nombre1;
+  }
+
+  public int getNombre2() {
+    return nombre2;
+  }
+
+  public void setNombre2(int nombre2) {
+    this.nombre2 = nombre2;
+  }
+}
